57755405b38902d046db8782e9fa29247bed9549
JohnCarbaugh <john.carbaugh@Outlook.com>
2018-10-03 12:07:45 -0400

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    mode: "development", // "production" | "development" | "none"
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    plugins: [
+        new OpenBrowserPlugin({ url: 'http://localhost:8080' })
+    ],
+
+    // When importing a module whose path matches on of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is imoportant because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
